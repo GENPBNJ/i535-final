cf3cb7d4b9686ee6d8def5e04b1ec4cbc051b580
GENPBNJ <45128565+GENPBNJ@users.noreply.github.com>
2023-04-23 18:25:26 -0500

    reboot

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
+import requests
 from flask import Flask
+from flask import jsonify
+
+
+instance_metadata_url = "http://metadata.google.internal/computeMetadata/v1/instance/"
+metadata_entries_to_grab = {"id": "instance-id", "name": "instance-name"}
+metadata_flavor_header = {"Metadata-Flavor": "Google"}
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Web App with Python Flask!'
 
-app.run(host='0.0.0.0', port=81)
+@app.route('/')
+def main():
+    return_resp = {"message": "hello world"}
+    return jsonify(return_resp)
